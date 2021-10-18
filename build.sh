bdc6f5f9d850896919297456a7744170301b93a2
Jiajie Chen <c@jia.je>
2021-10-18 08:17:22 +0800

    Bundle kernel and dts into FIT image

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+make ARCH=riscv CROSS_COMPILE=riscv64-linux-gnu- -j8 rocket-chip-vcu128_defconfig
+make ARCH=riscv CROSS_COMPILE=riscv64-linux-gnu- -j8 all
+cd arch/riscv/boot
+mkimage -f image.its image.itb
