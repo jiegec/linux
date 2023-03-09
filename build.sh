2e12f131e10bda20619c05e4cf1a4388dc848ccd
Jiajie Chen <c@jia.je>
2023-03-09 18:25:22 +0800

    Split single and dual core

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@ set -e
 make ARCH=riscv CROSS_COMPILE=riscv64-linux-gnu- -j8 rocket-chip-vcu128_defconfig
 make ARCH=riscv CROSS_COMPILE=riscv64-linux-gnu- -j8 all
 cd arch/riscv/boot
-mkimage -f image.its image.itb
+mkimage -f image-single-core.its image-single-core.itb
+mkimage -f image-dual-core.its image-dual-core.itb
 ls -alh ../../../vmlinux
